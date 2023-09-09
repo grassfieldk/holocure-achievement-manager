a4ab31a6a7f5cc79246f98971bcf21ba10038b9e
GrassfieldK <gtemacomp@gmail.com>
2023-09-09 12:54:26 +0900

    データを DB へ移行

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,13 +1,14 @@
+import { prisma } from '@/lib/prisma';
 import fs from 'fs';
-import { ACHIEVEMENTS } from '../../data/achievements';
 
-export default function Achievements({ params, searchParams }) {
+export default async function Achievements({ params, searchParams }) {
   if (!process.env.SAVEDATA) {
     console.error('セーブデータの読み込みに失敗しました');
     return <div>セーブデータの読み込みに失敗しました</div>;
   }
 
   const savedata = JSON.parse(Buffer.from(fs.readFileSync(process.env.SAVEDATA, 'utf8'), 'base64').toString('utf8'));
+  const achievements = await prisma.achievement.findMany();
   const filtered: boolean = searchParams.filtered;
 
   return (
@@ -19,14 +20,14 @@ export default function Achievements({ params, searchParams }) {
         <a href="/achievements?filtered=true">未クリアのみ表示</a>
       </p>
       <ul role="list" className="w-full max-w-screen-sm p-8 bg-white rounded-xl shadow-md divide-y divide-gray-200">
-        {ACHIEVEMENTS.map((achievement) => {
-          let unlocked = savedata.achievements[`${achievement.name}`].unlocked;
+        {achievements.map((achievement) => {
+          let unlocked = savedata.achievements[`${achievement.id}`].unlocked;
           if (filtered && unlocked) return;
           return (
-            <li key={achievement.name} className="flex justify-between gap-x-6 py-5">
+            <li key={achievement.id} className="flex justify-between gap-x-6 py-5">
               <div className="flex min-w-0 gap-x-4">
                 <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">{achievement.displayName}</p>
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{achievement.name}</p>
                   <p className="mt-1 truncate text-xs leading-5 text-gray-500">{achievement.requirement}</p>
                 </div>
               </div>
--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,35 +1,35 @@
 import fs from 'fs';
 import Image from 'next/image';
-import { GROUPS } from '../../data/characters';
+import { prisma } from '@/lib/prisma';
 
-export default function Characters() {
+export default async function Characters() {
   if (!process.env.SAVEDATA) {
     console.error('セーブデータの読み込みに失敗しました');
     return <div>セーブデータの読み込みに失敗しました</div>;
   }
 
+  const groups = await prisma.group.findMany({ include: { idols: true } });
   const savedata = JSON.parse(Buffer.from(fs.readFileSync(process.env.SAVEDATA, 'utf8'), 'base64').toString('utf8'));
-  console.log(savedata.achievements.ameClear);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul role="list" className="w-full max-w-screen-sm p-8 bg-white rounded-xl shadow-md divide-y divide-gray-200">
-        {GROUPS.map((group) =>
-          group.characters.map((character) => (
-            <li key={character.name} className="flex justify-between gap-x-6 py-2">
+        {groups.map((group) =>
+          group.idols.map((idol) => (
+            <li key={idol.name} className="flex justify-between gap-x-6 py-2">
               <div className="flex min-w-0 gap-x-4 justify-center items-center">
-                <Image width={43} height={38} src={`/portraits/${character.name}.png`} alt="" />
+                <Image width={43} height={38} src={`/portraits/${idol.id}.png`} alt="" />
                 <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">{character.displayName}</p>
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{idol.name}</p>
                   <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                    {group.area} / {group.displayName}
+                    {group.area} / {group.name}
                   </p>
                 </div>
               </div>
               <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                <p className="text-sm leading-6 text-gray-900">Lv. {[savedata.characters.find((c: string[]) => c[0] === character.name)[1]]}</p>
-                <p className={savedata.achievements[`${character.name}Clear`].unlocked ? 'text-primary' : 'text-gray-400'}>
-                  {savedata.achievements[`${character.name}Clear`].unlocked ? 'CLEARED!!' : 'NOT CLREAR..'}
+                <p className="text-sm leading-6 text-gray-900">Lv. {savedata.characters.find(c => c[0] === idol.id)[1]}</p>
+                <p className={savedata.achievements[`${idol.id}Clear`].unlocked ? 'text-primary' : 'text-gray-400'}>
+                  {savedata.achievements[`${idol.id}Clear`].unlocked ? 'CLEARED!!' : 'NOT CLREAR..'}
                 </p>
               </div>
             </li>
